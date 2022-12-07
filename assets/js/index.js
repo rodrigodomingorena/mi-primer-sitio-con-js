c2601f5b8f55f8f4ad4d9653ad98ef23ad545dd0
Rodrigo Domingorena <domingorenarodrigo@gmail.com>
2022-12-06 21:16:24 -0300

    Reestructuración temporal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,13 @@
-import { handlerControlOption } from "./handlers/control-option.js";
-import { headerFixed } from "./handlers/header-fixed.js";
+import { Header } from "./global/header.js";
 import { HandlerNutritionalInfo } from "./handlers/nutritional-info.js";
-import { rangeProgress } from "./range-progress/range-progress.js";
 
-window.addEventListener("scroll", headerFixed);
-
-window.addEventListener("resize", (event) => {
-  handlerControlOption.updateFixedScroll();
-});
-
-document.addEventListener("click", (event) => {
-  try {
-    handlerControlOption.click(event);
-  } catch (e) {}
+document.addEventListener("DOMContentLoaded", () => {
+  const header = new Header();
 });
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  headerFixed();
-
   try {
     const handlerNutritionalInfo = new HandlerNutritionalInfo();
     handlerNutritionalInfo.start();
-  } catch (e) {}
-
-  try {
-    rangeProgress();
   } catch (e) {}
 });
