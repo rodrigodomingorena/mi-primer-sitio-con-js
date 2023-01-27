dd8aa2f4407394787f45d16e0a90ac36595eb58b
Rodrigo Domingorena <domingorenarodrigo@gmail.com>
2023-01-26 21:10:33 -0300

    Plate dinámico

---
+++ assets/js/index-plate.js
@@ -0,0 +1,7 @@
+import { Header } from "./global/header.js";
+import { Index } from "./index-plate/Index.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const HEADER = new Header();
+  const INDEX = new Index();
+});
